f46e9b58030d81b019d550a8e129dc80b9d06ff6
Ene_Dimowo <78217379+Ene-420@users.noreply.github.com>
2023-10-14 01:01:31 +0100

    allow retrieval of form details

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function Book(title, author, pages, status){
 
     function info(){
       if(status === 'Currently Reading'){
-        return`${this.title} by ${this.author}, ${pages} pages is currently being read`;
+        return`${this.title} by ${this.author}, ${this.pages} pages is currently being read`;
       }
       else if(status ==='Finished'){ 
-        return`${this.title} by ${this.author}, ${pages} pages has been completed`;
+        return`${this.title} by ${this.author}, ${this.pages} pages has been completed`;
       }
 
       else{
-        return`${this.title} by ${this.author}, ${pages} pages has not been read`;
+        return`${this.title} by ${this.author}, ${this.pages} pages has not been read`;
       }
     }
   }
@@ -31,6 +31,21 @@ function Book(title, author, pages, status){
   libraryContent.push(book4);
 
 const contentBody = document.querySelector('.body');
+const dialog = document.getElementById('add_dialog');
+const addButton = document.querySelector('.add_icon');
+const formDetails = document.getElementById('book_form');
+const pageCount = document.getElementById('pages');
+const bookTitle = document.getElementById('title');
+const authorName = document.getElementById('author');
+
+
+
+addButton.addEventListener('click', ()=>{
+  dialog.showModal();
+});
+
+
+formDetails.addEventListener('submit', getBookDetails);
 
 for(let x =0; x< libraryContent.length; x++){
     let cardDiv = document.createElement('div');
@@ -52,4 +67,21 @@ for(let x =0; x< libraryContent.length; x++){
     cardDiv.appendChild(cardPage);
 
     contentBody.appendChild(cardDiv);
+}
+
+function getBookDetails(event){
+  const checkedValue = document.querySelector("input[type='radio'][name='status']:checked");
+
+  if(pageCount.value && authorName.value && bookTitle.value && checkedValue.value){
+    let createBook = new Book(bookTitle.value,authorName.value,pageCount.value, checkedValue.value);
+    console.log(createBook.info);
+    event.preventDefault();
+  }
+
+  else{
+    console.log('Invalid Details Entered');
+    console.log(checkedValue.value);
+    event.preventDefault();
+  }
+  
 }
