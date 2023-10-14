8ca523ac7c16f32c1848df423dffa9f7c1ecb098
Ene_Dimowo <78217379+Ene-420@users.noreply.github.com>
2023-10-15 00:18:18 +0100

    functional remove button

--- script.js
+++ script.js
@@ -33,41 +33,109 @@ function Book(title, author, pages, status){
 const contentBody = document.querySelector('.body');
 const dialog = document.getElementById('add_dialog');
 const addButton = document.querySelector('.add_icon');
-const formDetails = document.getElementById('book_form');
+const addBook = document.querySelector('.add-submit');
 const pageCount = document.getElementById('pages');
 const bookTitle = document.getElementById('title');
 const authorName = document.getElementById('author');
+const closeDialog = document.querySelector('.close_btn');
+
 
 
 
 addButton.addEventListener('click', ()=>{
+  dialog.style.visibility= 'visible';
   dialog.showModal();
+ 
 });
 
 
-formDetails.addEventListener('submit', getBookDetails);
+closeDialog.addEventListener('click', ()=>{
+  dialog.style.visibility= 'hidden';
+  dialog.close();
+})
 
-for(let x =0; x< libraryContent.length; x++){
-    let cardDiv = document.createElement('div');
-    cardDiv.classList.add('card');
+addBook.addEventListener('click', getBookDetails);
+updateContent();
 
-    let cardHeader = document.createElement('h2');
-    cardHeader.classList.add('title')
-    cardHeader.textContent = libraryContent[x].title;
+function updateContent(){
 
-    let cardAuthor = document.createElement('p');
-    cardAuthor.textContent = libraryContent[x].author;
 
-    let cardPage = document.createElement('p');
-    cardPage.textContent = `${libraryContent[x].pages} pages`;
-    cardPage.classList.add('page');
+    for(let x =0; x< libraryContent.length; x++){
+      let cardHeader = document.createElement('h2');
+      let cardAuthor = document.createElement('p');
+      let cardPage = document.createElement('p');
+      let removeBtn = document.createElement('button');
+      let continueBtn = document.createElement('button');
+      let cardDiv = document.createElement('div');
 
-    cardDiv.appendChild(cardHeader);
-    cardDiv.appendChild(cardAuthor);
-    cardDiv.appendChild(cardPage);
+      cardDiv.classList.add('card');
 
-    contentBody.appendChild(cardDiv);
+      
+      cardHeader.classList.add('title')
+      cardHeader.textContent = libraryContent[x].title;
+
+      
+      cardAuthor.textContent = libraryContent[x].author;
+
+      
+      cardPage.textContent = `${libraryContent[x].pages} pages`;
+      cardPage.classList.add('page');
+
+      let div = document.createElement('div');
+      div.classList.add('extraFeat');
+    
+      
+
+      removeBtn.textContent='remove';
+      removeBtn.classList.add('extra-btn');
+    
+      continueBtn.textContent='read';
+      continueBtn.classList.add('extra-btn');
+    
+      div.appendChild(removeBtn);
+      div.appendChild(continueBtn);
+    
+      
+
+      cardDiv.appendChild(cardHeader);
+      cardDiv.appendChild(cardAuthor);
+      cardDiv.appendChild(cardPage);
+      cardDiv.appendChild(div);
+
+      cardDiv.dataset.key =x;
+
+      contentBody.appendChild(cardDiv);
+  }
 }
+
+
+
+// const cards = document.querySelectorAll('.card');
+
+// cards.forEach((card) =>{
+//   card.addEventListener('mouseenter', extraBtnFunc(card))
+// });
+
+const extraButtons = document.querySelectorAll('.extra-btn');
+
+extraButtons.forEach(button =>{
+  button.addEventListener('click', ()=>{
+    console.log(button.textContent);
+
+    switch(button.textContent){
+      case 'remove':
+        removeBook(button.parentElement.parentElement.dataset.key);
+        button.parentElement.parentElement.remove();
+        //location.reload();
+        // updateContent();
+        break;
+      
+      case 'read':
+        console.log('Read button has been pressed');
+        break;
+    }
+  })
+})
 
 function getBookDetails(event){
   const checkedValue = document.querySelector("input[type='radio'][name='status']:checked");
@@ -84,4 +152,12 @@ function getBookDetails(event){
     event.preventDefault();
   }
   
+}
+
+function extraBtnFunc(element){
+
+}
+
+function removeBook(position){
+  libraryContent.splice(position,1);
 }
